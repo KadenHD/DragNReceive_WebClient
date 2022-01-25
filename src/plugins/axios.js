2fc2e9c57b22262c47d063304a29ba1b64187818
KadenHD <pierraored@gmail.com>
2022-01-25 20:43:19 +0100

    Update axios.js

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000/api/';
+const apiPORT = 3000;
+const apiURL = 'http://localhost:' + apiPORT + '/api/';
+
+axios.defaults.baseURL = apiURL;
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
