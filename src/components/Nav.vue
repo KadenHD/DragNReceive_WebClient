232635c5139c670dd50f2b0f9a8a6a0d269d966a
KadenHD <pierraored@gmail.com>
2022-01-26 18:40:06 +0100

    Nav done

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="Nav">
-    <router-link :to="{ name: 'Home' }">Home</router-link>
-    <router-link v-if="!user" :to="{ name: 'Login' }">Connexion</router-link>
-    <router-link v-if="!user" :to="{ name: 'Register' }"
-      >Inscription</router-link
-    >
-    <a v-if="user" href="javascript:void(0)" @click="handleClick"
-      >Déconnexion</a
-    >
+    <v-container>
+      <v-row>
+        <v-toolbar-title @click="$router.push({ name: 'Home' })">
+          <v-img
+            src="assets/img/nameAndLogo/logo.svg"
+            lazy-src="assets/img/nameAndLogo/logo.svg"
+            class="mx-auto"
+            max-width="100"
+          ></v-img>
+        </v-toolbar-title>
+
+        <v-divider class="mr-2 ml-2" inset vertical></v-divider>
+
+        <div class="links">
+          <div class="noUser" v-if="!user">
+            <router-link class="mr-1 ml-1" :to="{ name: 'Login' }"
+              >Connexion</router-link
+            >
+            <router-link class="mr-1 ml-1" :to="{ name: 'Register' }"
+              >Inscription</router-link
+            >
+          </div>
+          <div class="isUser" v-if="user">
+            <a class="mr-1 ml-1" href="javascript:void(0)" @click="logoutClick"
+              >Déconnexion</a
+            >
+          </div>
+        </div>
+      </v-row>
+    </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
