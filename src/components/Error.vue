ff95514f4b610b565e7a2437df4f68d80419398a
KadenHD <pierraored@gmail.com>
2022-01-26 13:32:36 +0100

    Update Error.vue

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,7 +1,9 @@
 <template>
-  <v-alert v-if="error" dismissible elevation="24" type="error">
-    {{ error }}
-  </v-alert>
+  <div class="Error">
+    <v-alert v-if="error" dismissible elevation="24" type="error">
+      {{ error }}
+    </v-alert>
+  </div>
 </template>
 
 <script>
