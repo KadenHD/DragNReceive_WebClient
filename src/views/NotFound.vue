da0c5f2b454e25961ac355ef81f0dfe4b7105276
KadenHD <pierraored@gmail.com>
2022-01-26 13:25:22 +0100

    Work on Not Found Page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>Erreur 404 - La route n'existe pas</div>
+  <div class="NotFound">
+    <v-btn class="font-weight-black" :to="{ name: 'Home' }"
+      >Retouner sur le site</v-btn
+    >
+    <v-img
+      src="assets/img/page_not_found.svg"
+      lazy-src="assets/img/page_not_found.svg"
+      class="mx-auto"
+      max-width="1250"
+    ></v-img>
+    <p
+      class="
+        font-weight-bold
+        text-xs-h6 text-sm-h5 text-md-h4 text-lg-h3 text-xl-h2
+      "
+    >
+      La route n'existe pas !
+    </p>
+  </div>
 </template>
-
-<script>
-export default {
-  name: "NotFound",
-};
-</script>
