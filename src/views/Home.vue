cdd6f5c5c84368de49792149f1e694564b439e62
KadenHD <pierraored@gmail.com>
2022-01-31 17:37:10 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,43 @@
 <template>
-  <div class="Home"></div>
+  <div class="Home">
+    <p class="title text-center">Bienvenue chez DragN'Receive !</p>
+    <v-divider class="mr-2 mb-6 ml-2" inset></v-divider>
+    <v-container>
+      <p class="text text-center">
+        Nous avons effectivement, moultes partenaires, nous essayons donc de
+        recruter uniquement "la crême" des vendeurs, de préférences des vendeurs
+        de produits artisanaux. Nous recrutons nos partenaire avec choix.
+      </p>
+      <v-img
+        src="../../public/assets/img/unDraw/working.svg"
+        lazy-src="../../public/assets/img/unDraw/working.svg"
+        class="mx-auto"
+        max-width="500"
+      ></v-img>
+      <v-divider class="mr-2 mb-6 ml-2" inset></v-divider>
+      <v-img
+        src="../../public/assets/img/Jimdo/DragNReceive/hexaInitial/logo.svg"
+        lazy-src="../../public/assets/img/Jimdo/DragNReceive/hexaInitial/logo.svg"
+        class="mx-auto"
+        max-width="100"
+      ></v-img>
+      <v-img
+        src="../../public/assets/img/Jimdo/DragNReceive/hexaInitial/logo.svg"
+        lazy-src="../../public/assets/img/Jimdo/DragNReceive/hexaInitial/logo.svg"
+        class="mx-auto"
+        max-width="100"
+      ></v-img>
+    </v-container>
+  </div>
 </template>
 
 <style >
+.Home .title {
+  color: #014d5a;
+  font-weight: bold;
+}
+.Home .text {
+  color: #4a7f88;
+  font-size: 16px;
+}
 </style>
