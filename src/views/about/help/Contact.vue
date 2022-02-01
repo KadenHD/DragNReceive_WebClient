8b0c019fe084913c8163068d31a10777a225e54b
KadenHD <pierraored@gmail.com>
2022-02-01 11:35:20 +0100

    Update Contact.vue

--- src/views/about/help/Contact.vue
+++ src/views/about/help/Contact.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="Contact">
-    <p class="title text-center">Nous contacter ?</p>
+    <p class="TextTitle title text-center">Nous contacter ?</p>
     <v-divider class="mr-2 mb-6 ml-2" inset></v-divider>
     <v-container>
-      <p class="text text-center">
+      <p class="MainText text-center">
         Vous pouvez nous contacter via ces coordonnées suivantes, nous restons
         également actif sur nos réseaux sociaux en messages privés.
       </p>
@@ -14,24 +14,9 @@
 
 <script>
 import CompanyDetails from "@/components/CompanyDetails.vue";
-
 export default {
   components: {
     CompanyDetails,
   },
 };
 </script>
-
-<style >
-.Contact .title {
-  color: #014d5a;
-  font-weight: bold;
-}
-.Contact .text {
-  color: #4a7f88;
-  font-size: 16px;
-}
-.Contact .label {
-  color: #afb854;
-}
-</style>
