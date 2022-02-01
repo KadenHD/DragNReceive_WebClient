30c6ba8711aa0acba433294931f0b03176d33a14
KadenHD <pierraored@gmail.com>
2022-02-01 11:35:27 +0100

    Update Policy.vue

--- src/views/about/company/Policy.vue
+++ src/views/about/company/Policy.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="Policy">
-    <p class="title text-center">Politique de confidentialité</p>
+    <p class="TextTitle title text-center">Politique de confidentialité</p>
     <v-divider class="mr-2 mb-6 ml-2" inset></v-divider>
     <v-container>
-      <p class="text text-center">
+      <p class="MainText text-center">
         Nous traitons les données de chacun de nos utilisateurs avec le plus
         grand soins, les connexion sont sécurisés, vos données personnels,
         factures et commandes sont privés et nous veillons à l'intégralité de la
@@ -19,18 +19,3 @@
     </v-container>
   </div>
 </template>
-
-<script>
-export default {};
-</script>
-
-<style >
-.Policy .title {
-  color: #014d5a;
-  font-weight: bold;
-}
-.Policy .text {
-  color: #4a7f88;
-  font-size: 16px;
-}
-</style>
