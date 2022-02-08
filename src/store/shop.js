17eccedb1286b671dfb77d1398f16ca5d7511812
KadenHD <pierraored@gmail.com>
2022-02-08 18:28:16 +0100

    Update shop.js

--- src/store/shop.js
+++ src/store/shop.js
@@ -1,22 +1,28 @@
-const state = {
-    shops: null,
-};
+import axios from 'axios';
+import router from '@/router';
 
 export default {
-    state,
+    state: {
+        shops: null,
+    },
     getters: {
-        shops: (state) => {
-            return state.shops;
-        }
+        shops: (state) => { return state.shops; }
     },
     actions: {
-        shops(context, shops) {
-            context.commit('shops', shops);
-        },
+        shops(context, shops) { context.commit('shops', shops); },
+        setShops(context) {
+            axios
+                .get("shops")
+                .then((response) => {
+                    context.commit("shops", response.data);
+                })
+                .catch((error) => {
+                    context.dispatch("error", error.response.data.error);
+                    router.push({ name: "Home" });
+                });
+        }
     },
     mutations: {
-        shops(state, shops) {
-            state.shops = shops;
-        },
+        shops(state, shops) { state.shops = shops; },
     }
 }
