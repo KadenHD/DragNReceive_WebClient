cde3610216c474248de7aa6eb3405d4098eabfac
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-02-11 15:04:03 +0100

    Update beforeEnterRoutes.js

--- src/plugins/beforeEnterRoutes.js
+++ src/plugins/beforeEnterRoutes.js
@@ -1,11 +1,17 @@
-// Voir le plus viable / unused
+import store from '@/store';
+
 export const isLogged = (to, from, next) => {
-    if (store.state.currentUser || localStorage.getItem('token')) next({ name: 'Home' });
+    if (!store.getters.currentUser || !localStorage.getItem('token')) next({ name: 'Home' });
     else next();
 }
 
 export const isNotLogged = (to, from, next) => {
-    if (store.state.currentUser || localStorage.getItem('token')) next({ name: 'Home' });
+    if (store.getters.currentUser || localStorage.getItem('token')) next({ name: 'Home' });
     else next();
 }
 
+export const homeRedirections = (to, from, next) => {
+    if (store.getters.currentUser.roleId == '1' || store.getters.currentUser.roleId == '2') next({ name: '' }); // sadmin or admin dashboard (/admin)
+    else if (store.getters.currentUser.roleId == '3') next({ name: '' }); // partner dashboard (/partner)
+    else next(); // not logged can access
+}
