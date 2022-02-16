3256a68430d12241e0d4c759a3ee65c381602107
KadenHD <pierraored@gmail.com>
2022-02-16 03:33:44 +0100

    Create Users.vue

---
+++ src/views/user/Users.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Users">
+    <v-data-table
+      :headers="headers"
+      :items="users"
+      :items-per-page="15"
+      class="elevation-1"
+    ></v-data-table>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  created() {
+    this.$store.dispatch("setUsers");
+  },
+  data() {
+    return {
+      headers: [
+        { text: "Nom", value: "firstname" },
+        { text: "Prénom", value: "lastname" },
+      ],
+    };
+  },
+  computed: {
+    ...mapGetters(["users"]),
+  },
+  methods: {},
+};
+</script>
