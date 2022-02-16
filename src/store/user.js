9fde4893b7cdfb2548bde42eb06fc6f00b6b9db1
KadenHD <pierraored@gmail.com>
2022-02-16 03:33:47 +0100

    Update user.js

--- src/store/user.js
+++ src/store/user.js
@@ -1,19 +1,31 @@
 import axios from 'axios';
+import router from '@/router';
 
 export default {
     state: {
-
+        users: null,
     },
     getters: {
-
+        users: (state) => { return state.users; }
     },
     actions: {
+        users(context, users) { context.commit('users', users); },
+        setUsers(context) {
+            axios
+                .get("users")
+                .then((response) => {
+                    context.commit("users", response.data);
+                })
+                .catch((error) => {
+                    context.dispatch("error", error.response.data.error);
+                });
+        },
         createUser(context, data) {
             axios
                 .post("users", data)
                 .then((response) => {
                     context.dispatch("success", response.data.success);
-                    //router.push({ name: "" }); // all Users page
+                    router.push({ name: "Users" });
                 })
                 .catch((error) => {
                     context.dispatch("error", error.response.data.error);
@@ -21,6 +33,6 @@ export default {
         }
     },
     mutations: {
-
+        users(state, users) { state.users = users; },
     }
 }
