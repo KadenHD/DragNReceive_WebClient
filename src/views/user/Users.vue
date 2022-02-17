b65dd7a542a822c5e086f75e126a215f2e9bf079
KadenHD <pierraored@gmail.com>
2022-02-17 17:54:28 +0100

    add viewDialog

--- src/views/user/Users.vue
+++ src/views/user/Users.vue
@@ -19,6 +19,84 @@
             </v-btn>
           </router-link>
 
+          <v-dialog v-model="dialogView" max-width="1000px">
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">Utilisateur n° {{ currentItem.id }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="currentItem.lastname"
+                        label="Nom"
+                        prepend-inner-icon="mdi-human-capacity-decrease"
+                        disabled
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="currentItem.firstname"
+                        label="Prénom"
+                        prepend-inner-icon="mdi-human-male"
+                        disabled
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="currentItem.email"
+                        label="E-mail"
+                        prepend-inner-icon="mdi-email"
+                        disabled
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="currentItem.roleName"
+                        label="Rôle"
+                        prepend-inner-icon="mdi-shield-crown"
+                        disabled
+                      ></v-text-field>
+                    </v-col>
+                    <v-col v-if="currentItem.shop" cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="currentItem.shop.name"
+                        label="Boutique"
+                        prepend-inner-icon="mdi-store"
+                        disabled
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="currentItem.createdAtReformated"
+                        label="Date de création"
+                        prepend-inner-icon="mdi-clock"
+                        disabled
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="currentItem.updatedAtReformated"
+                        label="Dernière modification"
+                        prepend-inner-icon="mdi-clock-outline"
+                        disabled
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeView">
+                  Fermer
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
           <v-dialog v-model="dialogEdit" max-width="1000px">
             <v-card>
               <v-card-title>
@@ -92,8 +170,10 @@
         </v-toolbar>
       </template>
       <template v-slot:item.actions="{ item }">
+        <v-icon class="mr-2" @click="viewItem(item)"> mdi-eye </v-icon>
         <v-icon class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
         <v-icon
+          class="mr-2"
           v-if="currentUser.roleId == '1' && item.roleId != '1'"
           @click="deleteItem(item)"
         >
@@ -119,6 +199,7 @@ export default {
     firstNameRules,
     lastNameRules,
     emailRules,
+    dialogView: false,
     dialogEdit: false,
     dialogDelete: false,
     currentIndex: -1,
@@ -163,6 +244,11 @@ export default {
   },
 
   methods: {
+    viewItem(item) {
+      this.currentIndex = this.userItems.indexOf(item);
+      this.currentItem = Object.assign({}, item);
+      this.dialogView = true;
+    },
     editItem(item) {
       this.currentIndex = this.userItems.indexOf(item);
       this.currentItem = Object.assign({}, item);
@@ -174,6 +260,13 @@ export default {
       this.dialogDelete = true;
     },
 
+    closeView() {
+      this.dialogView = false;
+      this.$nextTick(() => {
+        this.currentItem = Object.assign({}, null);
+        this.currentIndex = -1;
+      });
+    },
     closeEdit() {
       this.dialogEdit = false;
       this.$nextTick(() => {
