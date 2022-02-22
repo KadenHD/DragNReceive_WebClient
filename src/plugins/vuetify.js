783e68b2783323f80d836653161392810a34e7f8
KadenHD <pierraored@gmail.com>
2022-02-22 20:54:14 +0100

    Update vuetify.js

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,14 @@ import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#4a7f88',
+                success: '#4a7f88',
+                error: '#afb854',
+            },
+        },
+    },
+});
