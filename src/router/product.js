7d89e1a012fd51e025e5d1fd73b27f99ff95595b
KadenHD <pierraored@gmail.com>
2022-02-24 18:10:31 +0100

    Update product.js

--- src/router/product.js
+++ src/router/product.js
@@ -1,4 +1,5 @@
 import Products from '@/views/product/Products.vue';
+import ProductCreate from '@/views/product/ProductCreate.vue';
 
 import { isAdminOrSadmin, isPartner } from '@/functions/beforeEnter.js';
 
@@ -18,6 +19,15 @@ export default [
         component: Products,
         meta: {
             title: 'Gestion des boutiques'
+        },
+        beforeEnter: isPartner,
+    },
+    {
+        path: '/products/create',
+        name: 'ProductCreate',
+        component: ProductCreate,
+        meta: {
+            title: 'Créer produit'
         },
         beforeEnter: isPartner,
     },
