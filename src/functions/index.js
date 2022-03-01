1ea2561b5bb1329e73439c3bb4198b82e6943090
KadenHD <pierraored@gmail.com>
2022-03-01 21:31:09 +0100

    Update index.js

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,18 +1,16 @@
 export const reformatedDates = (key) => {
-    if (key.length == 24) {
-        const day = key[8] + key[9];
-        const month = key[5] + key[6];
-        const year = key[0] + key[1] + key[2] + key[3];
-        var hour = '';
-        if (parseInt(key[12]) === 9) {
-            hour = `${parseInt(key[11]) + 1}0`;
-        } else {
-            hour = `${key[11]}${parseInt(key[12]) + 1}`;
-        }
-        const minute = key[14] + key[15];
-        const second = key[17] + key[18];
-        return (`${day}/${month}/${year} - ${hour}:${minute}:${second}`)
-    } else return null
+    const day = key[8] + key[9];
+    const month = key[5] + key[6];
+    const year = key[0] + key[1] + key[2] + key[3];
+    var hour = '';
+    if (parseInt(key[12]) === 9) {
+        hour = `${parseInt(key[11]) + 1}0`;
+    } else {
+        hour = `${key[11]}${parseInt(key[12]) + 1}`;
+    }
+    const minute = key[14] + key[15];
+    const second = key[17] + key[18];
+    return (`${day}/${month}/${year} - ${hour}:${minute}:${second}`)
 }
 
 export const roledName = (key) => {
@@ -24,5 +22,5 @@ export const roledName = (key) => {
         return "Partenaire";
     } else if (key == "4") {
         return "Client";
-    } else return null
+    }
 }
