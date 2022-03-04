718a02d143f5faca3dfa8094697d7ed7c6a89296
KadenHD <pierraored@gmail.com>
2022-03-04 17:18:49 +0100

    Update Orders.vue

--- src/views/order/Orders.vue
+++ src/views/order/Orders.vue
@@ -6,6 +6,7 @@
 
 <script>
 import { mapGetters } from "vuex";
+import { reformatedDates, orderStatusName } from "@/functions/index.js";
 
 export default {
   data() {
@@ -15,7 +16,28 @@ export default {
   computed: {
     ...mapGetters(["orders", "currentUser"]),
     orderItems: function () {
-      return this.orders;
+      return this.orders.filter(function (i) {
+        for (let j = 0; j < i.orders.length; j++) {
+          i.orders[j].orderStatusName = orderStatusName(
+            i.orders[j].orderStatusId
+          );
+          i.orders[j].createdAtReformated = reformatedDates(
+            i.orders[j].createdAt
+          );
+          i.orders[j].updatedAtReformated = reformatedDates(
+            i.orders[j].updatedAt
+          );
+          i.orders[j].product.createdAtReformated = reformatedDates(
+            i.orders[j].product.createdAt
+          );
+          i.orders[j].product.updatedAtReformated = reformatedDates(
+            i.orders[j].product.updatedAt
+          );
+          i.user.createdAtReformated = reformatedDates(i.user.createdAt);
+          i.user.updatedAtReformated = reformatedDates(i.user.updatedAt);
+        }
+        return i;
+      });
     },
   },
 
