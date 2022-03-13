a0408064610b7fe4028f91e7e50cef8b444ed2b9
KadenHD <pierraored@gmail.com>
2022-03-14 00:19:09 +0100

    Update order.js

--- src/store/order.js
+++ src/store/order.js
@@ -1,14 +1,29 @@
 import axios from 'axios';
+import router from '@/router';
 
 export default {
     state: {
         orders: null,
+        order: null,
     },
     getters: {
         orders: (state) => { return state.orders; },
+        order: (state) => { return state.order; },
     },
     actions: {
         orders(context, orders) { context.commit('orders', orders); },
+        order(context, order) { context.commit('order', order); },
+        setOrder(context, number) {
+            axios
+                .get("orders/" + number)
+                .then((response) => {
+                    context.commit("order", response.data);
+                })
+                .catch((error) => {
+                    context.dispatch("alertError", error.response.data.error);
+                    router.push({ name: "Home" });
+                });
+        },
         setOrders(context) {
             axios
                 .get("orders")
@@ -22,5 +37,6 @@ export default {
     },
     mutations: {
         orders(state, orders) { state.orders = orders; },
+        order(state, order) { state.order = order; },
     }
 }
