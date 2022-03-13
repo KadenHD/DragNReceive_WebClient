cadbec4c61f6aec453120ef4d8bdd86249341592
KadenHD <pierraored@gmail.com>
2022-03-14 00:19:07 +0100

    Update Order.vue

--- src/views/order/Order.vue
+++ src/views/order/Order.vue
@@ -1,10 +1,40 @@
 <template>
-  <div class="Order"></div>
+  <div class="Order">
+    {{ orderItems }}
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+import { mapGetters } from "vuex";
+import { reformatedDates, orderStatusName } from "@/functions/index.js";
 
-<style>
-</style>
+export default {
+  data() {
+    return {};
+  },
+
+  created() {
+    this.$store.dispatch("setOrder", this.$route.params.number);
+  },
+
+  computed: {
+    ...mapGetters(["order", "currentUser"]),
+    orderItems: function () {
+      console.log(this.order);
+      return this.order.filter(function (i) {
+        i.priceEuro = i.price + " €";
+        i.product.path = process.env.VUE_APP_URL + i.product.path;
+        i.orderStatusName = orderStatusName(i.orderStatusId);
+        i.createdAtReformated = reformatedDates(i.createdAt);
+        i.updatedAtReformated = reformatedDates(i.updatedAt);
+        i.product.createdAtReformated = reformatedDates(i.product.createdAt);
+        i.product.updatedAtReformated = reformatedDates(i.product.updatedAt);
+        i.orderStatusName = orderStatusName(i.status);
+        i.user.createdAtReformated = reformatedDates(i.user.createdAt);
+        i.user.updatedAtReformated = reformatedDates(i.user.updatedAt);
+        return i;
+      });
+    },
+  },
+};
+</script>
